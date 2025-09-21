b42d8a29503f00b69da7d9813da0318f9d162980
Andrzej Ressel <github@andrzejressel.pl>
2025-09-21 23:32:14 +0200

    Manage ruleset using Pulumi

---
+++ deploy/repository/src/github_workflow.rs
@@ -0,0 +1,155 @@
+use anyhow::Context;
+use anyhow::Result;
+use itertools::Itertools;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct GitHubWorkflow {
+    pub jobs: HashMap<String, Job>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct Job {
+    pub name: Option<String>,
+    pub strategy: Option<Strategy>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct Strategy {
+    pub matrix: Option<HashMap<String, Vec<String>>>,
+}
+
+impl GitHubWorkflow {
+    pub fn from_file(path: &str) -> Result<Self> {
+        let content = std::fs::read_to_string(path)
+            .with_context(|| format!("Failed to read file {}", path))?;
+        let workflow: GitHubWorkflow = Self::from_yaml(&content)
+            .with_context(|| format!("Failed to parse file {}", path))?;
+        Ok(workflow)
+    }
+    
+    pub fn from_yaml(yaml_content: &str) -> Result<Self> {
+        serde_yaml::from_str(yaml_content).context("Failed to parse YAML content")
+    }
+
+    pub fn get_job_full_names(&self) -> Vec<String> {
+        let mut job_names = Vec::new();
+
+        for (job_name, job) in &self.jobs {
+            let job_name = job.name.clone().unwrap_or_else(|| job_name.clone());
+            if let Some(matrix) = job.strategy.as_ref().and_then(|s| s.matrix.as_ref())
+                && !matrix.is_empty()
+            {
+                let combinations = self.generate_matrix_combinations(matrix);
+                for combo in combinations {
+                    job_names.push(format!("{} ({})", job_name, combo));
+                }
+            } else {
+                // Job without matrix
+                job_names.push(job_name.clone());
+            }
+        }
+
+        job_names.sort();
+        job_names
+    }
+
+    fn generate_matrix_combinations(&self, matrix: &HashMap<String, Vec<String>>) -> Vec<String> {
+        matrix
+            .values()
+            .multi_cartesian_product()
+            .map(|t| t.into_iter().join(" "))
+            .collect_vec()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const YAML_CONTENT: &str = r#"
+jobs:
+  build-base:
+    strategy:
+      matrix:
+        os: [ubuntu-24.04, windows-2022, macos-14]
+  build-generated-provider:
+    strategy:
+      matrix:
+        provider: [cloudflare, docker, aws-0, aws-1]
+  build-no-matrix:
+    job: Build no matrix
+    runs-on: ubuntu-latest
+"#;
+
+    #[test]
+    fn test_parse_workflow() {
+        let workflow = GitHubWorkflow::from_yaml(YAML_CONTENT).unwrap();
+
+        let expected = GitHubWorkflow {
+            jobs: HashMap::from([
+                (
+                    "build-base".to_string(),
+                    Job {
+                        name: None,
+                        strategy: Some(Strategy {
+                            matrix: Some(HashMap::from([(
+                                "os".to_string(),
+                                vec![
+                                    "ubuntu-24.04".to_string(),
+                                    "windows-2022".to_string(),
+                                    "macos-14".to_string(),
+                                ],
+                            )])),
+                        }),
+                    },
+                ),
+                (
+                    "build-generated-provider".to_string(),
+                    Job {
+                        name: None,
+                        strategy: Some(Strategy {
+                            matrix: Some(HashMap::from([(
+                                "provider".to_string(),
+                                vec![
+                                    "cloudflare".to_string(),
+                                    "docker".to_string(),
+                                    "aws-0".to_string(),
+                                    "aws-1".to_string(),
+                                ],
+                            )])),
+                        }),
+                    },
+                ),
+                (
+                    "build-no-matrix".to_string(),
+                    Job {
+                        name: Some("Build no matrix".to_string()),
+                        strategy: None,
+                    },
+                ),
+            ]),
+        };
+
+        assert_eq!(workflow, expected);
+    }
+
+    #[test]
+    fn test_generate_full_job_names() {
+        let workflow = GitHubWorkflow::from_yaml(YAML_CONTENT).unwrap();
+
+        let job_names = workflow.get_job_full_names();
+
+        let expected_job_names = [
+            "build-base (macos-14)",
+            "build-base (ubuntu-24.04)",
+            "build-base (windows-2022)",
+            "build-generated-provider (aws-0)",
+            "build-generated-provider (aws-1)",
+            "build-generated-provider (cloudflare)",
+            "build-generated-provider (docker)",
+            "Build no matrix",
+        ];
+    }
+}
--- deploy/repository/src/main.rs
+++ deploy/repository/src/main.rs
@@ -1,3 +1,4 @@
+use std::ops::Deref;
 use crate::github::functions::get_repository::GetRepositoryArgs;
 use crate::github::repository_ruleset::RepositoryRulesetArgs;
 use crate::github::types::{
@@ -10,6 +11,7 @@ use github::types::RepositoryRulesetRulesRequiredStatusChecks;
 use pulumi_gestalt_rust::Context;
 
 mod github;
+mod github_workflow;
 
 pulumi_gestalt_rust::pulumi_main!();
 
@@ -39,6 +41,24 @@ fn pulumi_main(ctx: &Context) -> Result<()> {
             .build_struct(),
     );
 
+    let build_yaml_jobs =
+        github_workflow::GitHubWorkflow::from_file("../../.github/workflows/build.yml")?
+            .get_job_full_names();
+    let codeql_yaml_jobs =
+        github_workflow::GitHubWorkflow::from_file("../../.github/workflows/codeql.yml")?
+            .get_job_full_names();
+    let rust_clippy_jobs =
+        github_workflow::GitHubWorkflow::from_file("../../.github/workflows/rust-clippy.yml")?
+            .get_job_full_names();
+
+    let all_jobs = [build_yaml_jobs, codeql_yaml_jobs, rust_clippy_jobs].concat();
+
+    let pulumi_gestalt_checks: Vec<RepositoryRulesetRulesRequiredStatusChecksRequiredCheck> =
+        all_jobs
+            .iter()
+            .map(|job_name| create_check(job_name, GithubActions))
+            .collect();
+
     github::repository_ruleset::create(
         ctx,
         "main-protection-2",
@@ -59,181 +79,29 @@ fn pulumi_main(ctx: &Context) -> Result<()> {
             )
             .rules(
                 RepositoryRulesetRules::builder()
-                    .deletion(Box::new(Some(true)))
-                    .non_fast_forward(Box::new(Some(true)))
-                    .required_linear_history(Box::new(Some(true)))
-                    .pull_request(Box::new(Some(
+                    .deletion(Some(true))
+                    .non_fast_forward(Some(true))
+                    .required_linear_history(Some(true))
+                    .pull_request(Some(
                         RepositoryRulesetRulesPullRequest::builder().build_struct(),
-                    )))
-                    .required_status_checks(Box::new(Some(
+                    ))
+                    .required_status_checks(Some(
                         RepositoryRulesetRulesRequiredStatusChecks::builder()
-                            .strict_required_status_checks_policy(Box::new(Some(false)))
-                            .required_checks(Box::new(vec![
-                                // Any
-                                create_check("docs/readthedocs.org:pulumi-gestalt", Any),
-                                // GithubActions
-                                create_check("Analyze C", GithubActions),
-                                create_check("Analyze Go", GithubActions),
-                                create_check("build-base (ubuntu-24.04)", GithubActions),
-                                create_check("build-base (windows-2022)", GithubActions),
-                                create_check("build-c (ubuntu-24.04)", GithubActions),
-                                create_check("build-c (windows-2022)", GithubActions),
-                                create_check("build-examples (ubuntu-24.04)", GithubActions),
-                                create_check("build-examples (windows-2022)", GithubActions),
-                                create_check(
-                                    "build-generated-provider (array-of-enum-map)",
-                                    GithubActions,
-                                ),
-                                create_check("build-generated-provider (aws-0)", GithubActions),
-                                create_check("build-generated-provider (aws-1)", GithubActions),
-                                create_check("build-generated-provider (aws-2)", GithubActions),
-                                create_check("build-generated-provider (aws-3)", GithubActions),
-                                create_check("build-generated-provider (aws-4)", GithubActions),
-                                create_check("build-generated-provider (aws-5)", GithubActions),
-                                create_check("build-generated-provider (aws-6)", GithubActions),
-                                create_check("build-generated-provider (aws-7)", GithubActions),
-                                create_check("build-generated-provider (aws-8)", GithubActions),
-                                create_check("build-generated-provider (aws-9)", GithubActions),
-                                create_check("build-generated-provider (aws-10)", GithubActions),
-                                create_check("build-generated-provider (aws-11)", GithubActions),
-                                create_check("build-generated-provider (aws-12)", GithubActions),
-                                create_check("build-generated-provider (aws-13)", GithubActions),
-                                create_check("build-generated-provider (aws-14)", GithubActions),
-                                create_check("build-generated-provider (aws-15)", GithubActions),
-                                create_check("build-generated-provider (aws-16)", GithubActions),
-                                create_check("build-generated-provider (aws-17)", GithubActions),
-                                create_check("build-generated-provider (aws-18)", GithubActions),
-                                create_check("build-generated-provider (aws-19)", GithubActions),
-                                create_check("build-generated-provider (aws-20)", GithubActions),
-                                create_check("build-generated-provider (aws-21)", GithubActions),
-                                create_check("build-generated-provider (azure-0)", GithubActions),
-                                create_check("build-generated-provider (azure-1)", GithubActions),
-                                create_check("build-generated-provider (azure-2)", GithubActions),
-                                create_check("build-generated-provider (azure-3)", GithubActions),
-                                create_check("build-generated-provider (azure-4)", GithubActions),
-                                create_check("build-generated-provider (azure-5)", GithubActions),
-                                create_check("build-generated-provider (azure-6)", GithubActions),
-                                create_check("build-generated-provider (azure-7)", GithubActions),
-                                create_check("build-generated-provider (azure-8)", GithubActions),
-                                create_check("build-generated-provider (azure-9)", GithubActions),
-                                create_check("build-generated-provider (azure-10)", GithubActions),
-                                create_check("build-generated-provider (azure-11)", GithubActions),
-                                create_check("build-generated-provider (azure-12)", GithubActions),
-                                create_check("build-generated-provider (azure-13)", GithubActions),
-                                create_check(
-                                    "build-generated-provider (cloudflare)",
-                                    GithubActions,
-                                ),
-                                create_check(
-                                    "build-generated-provider (cyclic-types)",
-                                    GithubActions,
-                                ),
-                                create_check(
-                                    "build-generated-provider (different-enum)",
-                                    GithubActions,
-                                ),
-                                create_check("build-generated-provider (docker)", GithubActions),
-                                create_check(
-                                    "build-generated-provider (filtering-0)",
-                                    GithubActions,
-                                ),
-                                create_check(
-                                    "build-generated-provider (filtering-1)",
-                                    GithubActions,
-                                ),
-                                create_check(
-                                    "build-generated-provider (filtering-2)",
-                                    GithubActions,
-                                ),
-                                create_check(
-                                    "build-generated-provider (functions-secrets)",
-                                    GithubActions,
-                                ),
-                                create_check("build-generated-provider (gcp-0)", GithubActions),
-                                create_check("build-generated-provider (gcp-1)", GithubActions),
-                                create_check("build-generated-provider (gcp-2)", GithubActions),
-                                create_check("build-generated-provider (gcp-3)", GithubActions),
-                                create_check("build-generated-provider (gcp-4)", GithubActions),
-                                create_check("build-generated-provider (gcp-5)", GithubActions),
-                                create_check("build-generated-provider (gcp-6)", GithubActions),
-                                create_check("build-generated-provider (gcp-7)", GithubActions),
-                                create_check("build-generated-provider (gcp-8)", GithubActions),
-                                create_check("build-generated-provider (gcp-9)", GithubActions),
-                                create_check("build-generated-provider (gcp-10)", GithubActions),
-                                create_check("build-generated-provider (gcp-11)", GithubActions),
-                                create_check("build-generated-provider (gcp-12)", GithubActions),
-                                create_check("build-generated-provider (gcp-13)", GithubActions),
-                                create_check(
-                                    "build-generated-provider (housekeeping (ubuntu-24.04))",
-                                    GithubActions,
-                                ),
-                                create_check(
-                                    "build-generated-provider (housekeeping (windows-2022))",
-                                    GithubActions,
-                                ),
-                                create_check(
-                                    "build-generated-provider (mini-awsnative)",
-                                    GithubActions,
-                                ),
-                                create_check(
-                                    "build-generated-provider (nested-module)",
-                                    GithubActions,
-                                ),
-                                create_check(
-                                    "build-generated-provider (nested-module-thirdparty)",
-                                    GithubActions,
-                                ),
-                                create_check(
-                                    "build-generated-provider (output-funcs)",
-                                    GithubActions,
-                                ),
-                                create_check(
-                                    "build-generated-provider (output-funcs-edgeorder)",
-                                    GithubActions,
-                                ),
-                                create_check(
-                                    "build-generated-provider (plain-object-defaults)",
-                                    GithubActions,
-                                ),
-                                create_check(
-                                    "build-generated-provider (plain-object-disable-defaults)",
-                                    GithubActions,
-                                ),
-                                create_check("build-generated-provider (random)", GithubActions),
-                                create_check(
-                                    "build-generated-provider (reserved_names)",
-                                    GithubActions,
-                                ),
-                                create_check(
-                                    "build-generated-provider (unions-inline)",
-                                    GithubActions,
-                                ),
-                                create_check(
-                                    "build-generated-provider (unions-inside-arrays)",
-                                    GithubActions,
-                                ),
-                                create_check("build-housekeeping (ubuntu-24.04)", GithubActions),
-                                create_check("build-housekeeping (windows-2022)", GithubActions),
-                                create_check("build-native (ubuntu-24.04)", GithubActions),
-                                create_check("build-native (windows-2022)", GithubActions),
-                                create_check("build-rust-docs", GithubActions),
-                                create_check("clippy", GithubActions),
-                                create_check(
-                                    "Run rust-clippy analyzing (ubuntu-24.04)",
-                                    GithubActions,
-                                ),
-                                create_check(
-                                    "Run rust-clippy analyzing (windows-2022)",
-                                    GithubActions,
-                                ),
-                                // GithubAdvancedSecurity
-                                create_check("CodeQL", GithubAdvancedSecurity),
-                                create_check("clippy", GithubAdvancedSecurity),
-                                // Mergify
-                                create_check("Mergify Merge Protections", Mergify),
-                            ]))
+                            .strict_required_status_checks_policy(Some(false))
+                            .required_checks(
+                                [
+                                    &[
+                                        create_check("docs/readthedocs.org:pulumi-gestalt", Any),
+                                        create_check("CodeQL", GithubAdvancedSecurity),
+                                        create_check("clippy", GithubAdvancedSecurity),
+                                        create_check("Mergify Merge Protections", Mergify),
+                                    ],
+                                    pulumi_gestalt_checks.deref(),
+                                ]
+                                .concat(),
+                            )
                             .build_struct(),
-                    )))
+                    ))
                     .build_struct(),
             )
             .build_struct(),
@@ -248,6 +116,6 @@ fn create_check(
 ) -> RepositoryRulesetRulesRequiredStatusChecksRequiredCheck {
     RepositoryRulesetRulesRequiredStatusChecksRequiredCheck::builder()
         .context(name.to_string())
-        .integration_id(Box::new(Some(integration.get_integration_id())))
+        .integration_id(Some(integration.get_integration_id()))
         .build_struct()
 }
