1010d464f7a2652329427ed66e24b3365bed8581
Andrzej Ressel <github@andrzejressel.pl>
2026-04-14 21:30:10 +0200

    Update

--- pulumi-language-rust/testdata/projects/l1-builtin-require-pulumi-version/src/main.rs
+++ pulumi-language-rust/testdata/projects/l1-builtin-require-pulumi-version/src/main.rs
@@ -6,7 +6,6 @@ fn pulumi_main(ctx: &pulumi_gestalt_rust::Context) -> Result<()> {
     let version = ctx
         .require_config(None, "version")
         .expect("Expected config [version] to exist");
-    ctx.require_pulumi_version(&version)
-        .expect("Failed to require Pulumi version");
+    ctx.require_pulumi_version(&version).expect("Failed to require Pulumi version");
     Ok(())
 }
