ed23b4f095ddba9acb684c581825829177d2d1f6
Andrzej Ressel <github@andrzejressel.pl>
2026-05-10 23:00:07 +0200

    New base Pulumi model

--- crates/model_macros/tests/macro_test.rs
+++ crates/model_macros/tests/macro_test.rs
@@ -9,18 +9,20 @@ fn test_derive_struct() {
         a: String,
         b: i64,
         c: Output<bool>,
+        d: Option<String>
     }
 
     let val = MyStruct {
         a: "hello".to_string(),
         b: 42,
         c: Output::new_secret(true),
+        d: None
     };
 
     let pv = block_on(val.to_pulumi_value());
 
     if let PulumiValueContent::Object(obj) = pv.content {
-        assert_eq!(obj.len(), 3);
+        assert_eq!(obj.len(), 4);
         assert_eq!(obj[0].0, "a");
         assert_eq!(
             obj[0].1.content,
@@ -30,6 +32,8 @@ fn test_derive_struct() {
         assert_eq!(obj[1].1.content, PulumiValueContent::Integer(42));
         assert_eq!(obj[2].0, "c");
         assert_eq!(obj[2].1.content, PulumiValueContent::Boolean(true));
+        assert_eq!(obj[3].0, "d");
+        assert_eq!(obj[3].1.content, PulumiValueContent::None);
         assert!(pv.secret); // Because c is secret
     } else {
         panic!("Expected Object");
